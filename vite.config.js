dfa3e828461bb06544eb4595a89fc177181a6371
salino3 <flaviobcn012@gmail.com>
2023-04-02 15:38:56 +0200

    ssh 11

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+
 export default defineConfig({
   base: './',
   envPrefix: 'PUBLIC_',
